71e55858a78a70b10364c93f5be2eee31607d8a1
tris <tris.bouden@gmail.com>
2024-06-03 16:47:47 +0100

    commit solitaire code

---
+++ src/SolitairePuzzle.tsx
@@ -0,0 +1,321 @@
+// SolitairePuzzle.tsx
+import React from 'react';
+
+import { PuzzleProps } from 'drunkmode-puzzles';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+import Foundation from './components/Foundation';
+import {
+  checkWinningCondition,
+  handleDragEnd,
+  handleMultipleDrag,
+  moveCardsAfterWin,
+  removeTranslate,
+} from './components/Logic';
+import StockPile from './components/Stock';
+import Tableau from './components/Tableau';
+
+export const SUITS = {
+  club: './club.png',
+  diamond: './diamond.png',
+  heart: './heart.png',
+  spade: './spade.png',
+} as const;
+
+export type Suit = keyof typeof SUITS;
+
+// Define card suits and ranks
+export const RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] as const;
+
+export type Rank = typeof RANKS[number];
+
+export type CardProps = {
+  id: `${Suit}${Rank}`;
+  suit: Suit;
+  rank: Rank;
+  isFaceUp: boolean;
+  red: boolean;
+};
+
+// Generate all 52 cards
+const DECK: CardProps[] = [];
+for (const suit of Object.keys(SUITS) as Suit[]) {
+  for (const rank of RANKS) {
+    DECK.push({
+      id: `${suit}${rank}`,
+      isFaceUp: true,
+      rank,
+      red: suit === 'heart' || suit === 'diamond',
+      suit,
+    });
+  }
+}
+
+export type HistoryState = {
+  flippedCards: CardProps[];
+  stockCards: CardProps[];
+  suitCards: CardProps[][];
+  tableauCards: CardProps[][];
+  wasteCards: CardProps[];
+};
+
+export const Puzzle = ({
+  startFresh,
+  data,
+  onProgress,
+  onSuccess,
+  ...props
+}: PuzzleProps) => {
+  
+  const [loaded, setLoaded] = React.useState(false);
+  const [tableauCards, setTableauCards] = React.useState<CardProps[][]>([]);
+  const [wasteCards, setWasteCards] = React.useState<CardProps[]>([]); 
+  const [stockCards, setStockCards] = React.useState<CardProps[]>([]);
+  const [suitCards, setSuitCards] = React.useState<CardProps[][]>([[], [], [], []]);
+  const [_, setGameHistory] = React.useState<HistoryState[]>([]);
+  const [isDragging, setIsDragging] = React.useState(false);
+  const setCards = React.useCallback(() => {
+    const shuffledCards = [...DECK].sort(() => Math.random() - 0.5);
+    setTableauCards(() => {
+      const cards: CardProps[][] = [];
+      let currentIndex = 0;
+      for (let i = 1; i <= 7; i++) {
+        const column = [];
+        for (let j = 0; j < i; j++) {
+          if (j+1 == i) {
+            shuffledCards.slice(0, 28)[currentIndex + j].isFaceUp = true;
+          } else {
+            shuffledCards.slice(0, 28)[currentIndex + j].isFaceUp = false;
+          }
+          column.push(shuffledCards.slice(0, 28)[currentIndex + j]);
+        }
+        cards.push(column);
+        currentIndex += i;
+      }
+
+      const newStates: HistoryState = {
+        flippedCards: [],
+        stockCards: shuffledCards.slice(28), 
+        suitCards, 
+        tableauCards: cards, 
+        wasteCards,
+      };
+      setGameHistory((prev) => {
+        const state = [...(prev ?? []), newStates];
+        onProgress?.(JSON.stringify(state));
+        return state;
+      });
+
+      return cards;
+    }); 
+    setStockCards(shuffledCards.slice(28));
+  }, []);
+  
+  React.useEffect(() => {
+    if (loaded) {
+      return;
+    }
+    setLoaded(true);
+    if (data && !startFresh) {
+      try {
+        const history = typeof data === 'string' ? JSON.parse(data) : data;
+        setGameHistory(history);
+      } catch (e) {
+        alert('there was an issue loading puzzle progress');
+        setCards();
+      }
+    } else {
+      setCards();
+    }
+  }, [startFresh, data, loaded, setCards]);
+  
+  const undoMove = React.useCallback(() => {
+    if (!loaded) {
+      return;
+    }
+    setGameHistory((prev) => {
+      if (prev.length > 1) {
+        const curr = prev[prev.length - 1];
+        const old = prev[prev.length - 2];
+        for (const column of old.tableauCards) {
+          for (const card of column) {
+            if (curr.flippedCards.some((c) => c.id === card.id)) {
+              card.isFaceUp = false;
+            }
+          }
+        }
+        setTableauCards(old.tableauCards);
+        setWasteCards(old.wasteCards);
+        setStockCards(old.stockCards);
+        setSuitCards(old.suitCards);
+        return prev.slice(0, -1);
+      }
+      return prev;
+    });
+  }, [loaded]);
+  
+  const resetGame = () => {
+    const reset = confirm('Are you sure you want to start a new game? All progress will be lost and cannot be done');
+    if (!reset) {
+      return;
+    }
+    setGameHistory([]);
+    setTableauCards([]);
+    setWasteCards([]);
+    setStockCards([]);
+    setSuitCards([[], [], [], []]);
+    setCards();
+  };
+  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleDragEndWrapper = React.useCallback(({ source, destination }: { source: any; destination: any; }) => {
+    
+    const {
+      failure,
+      uFlipped, 
+      uTableau, 
+      uSuit,
+      uWaste, 
+    } = handleDragEnd({
+      destination,
+      source,
+      suitCards,
+      tableauCards,
+      wasteCards,
+    });
+
+    const newStates: HistoryState = {
+      flippedCards: uFlipped,
+      stockCards, 
+      suitCards: uSuit, 
+      tableauCards: uTableau, 
+      wasteCards: uWaste,
+    };
+    
+    setGameHistory((prev) => {
+      const isWinner = checkWinningCondition(uTableau, uWaste, stockCards);
+      if (isWinner && prev.length > 2) {
+        moveCardsAfterWin({
+          setSuitCards, 
+          setTableauCards, 
+          suitCards: uSuit,
+          tableauCards: uTableau,
+        });
+        setTableauCards([])
+        onSuccess();
+      }
+      const state = [...(prev ?? []), newStates];
+      onProgress?.(JSON.stringify(state));
+      return state;
+    });
+
+    setTableauCards(uTableau);
+    setSuitCards(uSuit);
+    setWasteCards(uWaste);
+    setIsDragging(false);
+    removeTranslate(tableauCards);
+  }, [onProgress, onSuccess, stockCards, suitCards, tableauCards, wasteCards]);
+  
+  const handleMultiple = (source: any) => {
+    setIsDragging(source);
+  };
+  
+  const handleOnDrawCard = () => {
+    setStockCards((prev) => {
+      if (prev.length > 0) {
+        const lastCard = prev[prev.length - 1]; 
+        lastCard.isFaceUp = true;
+        setWasteCards((prev) => [...prev, lastCard]);
+        const newStates: HistoryState = {
+          flippedCards: [],
+          stockCards: prev.slice(0, -1), 
+          suitCards, 
+          tableauCards, 
+          wasteCards: [...wasteCards, lastCard],
+        };
+    
+        setGameHistory((prev) => {
+          
+          const state = [...(prev ?? []), newStates];
+          onProgress?.(JSON.stringify(state));
+          return state;
+        });
+        return prev.slice(0, -1);
+        
+      } else {
+        const resetWasteCards = wasteCards.map(card => ({ ...card, isFaceUp: false })).reverse();
+        setWasteCards([]); 
+        const newStates: HistoryState = {
+          flippedCards: [],
+          stockCards: resetWasteCards, 
+          suitCards, 
+          tableauCards, 
+          wasteCards: [],
+        };
+    
+        setGameHistory((prev) => {
+          
+          const state = [...(prev ?? []), newStates];
+          onProgress?.(JSON.stringify(state));
+          return state;
+        });
+        return resetWasteCards;
+      }
+    });
+  };
+  const isDrag = React.useCallback(() => {
+    handleMultipleDrag(isDragging, tableauCards);
+  }, [isDragging, tableauCards]);
+
+  React.useEffect(() => {
+    if (isDragging) {
+      window.addEventListener('touchmove', isDrag);
+      window.addEventListener('mousemove', isDrag);
+    } else {
+      window.removeEventListener('touchmove', isDrag);
+      window.removeEventListener('mousemove', isDrag);
+    }
+
+    return () => {
+      window.removeEventListener('touchmove', isDrag);
+      window.removeEventListener('mousemove', isDrag);
+    };
+  }, [isDragging, isDrag]);
+  return (
+    <div
+      className="flex flex-col px-2 w-full max-w-screen-lg mx-auto min-h-screen justify-center overscroll-none select-none"
+      style={ { flexGrow: 1 } }>
+      {props.preview && (
+        <React.Fragment>
+        </React.Fragment>
+      )}
+      {/* Foundation component */}
+      <DragDropContext
+        onDragEnd={ handleDragEndWrapper }
+        onDragStart={ handleMultiple }>
+        <div className='flex justify-between'>
+          <StockPile 
+            cards={ stockCards }
+            wasteCards={ wasteCards }
+            onDrawCard={ handleOnDrawCard } />
+          <Foundation 
+            suits={ Object.values(SUITS) }
+            suitCards={ suitCards } /> 
+        </div>
+        <Tableau tableauCards={ tableauCards } />
+      </DragDropContext>
+      <div className="flex justify-center gap-6 mb-2">
+        <button 
+          className='px-3 py-2 rounded-sm duration-300 cursor-pointer hover:bg-red-700/60'
+          onClick={ undoMove }>
+          UNDO
+        </button>
+        <button
+          className='px-3 py-2 rounded-sm duration-300 cursor-pointer hover:bg-red-700/60'
+          onClick={ resetGame }>
+          NEW
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Logic.ts
@@ -0,0 +1,240 @@
+// Logic.ts
+import {
+  CardProps,
+  RANKS,
+  SUITS,
+} from '~/SolitairePuzzle';
+
+export const handleDragEnd = ({
+  source,
+  destination,
+  tableauCards,
+  suitCards,
+  wasteCards,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  source: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  destination: any;
+  tableauCards: CardProps[][];
+  suitCards: Array<CardProps[]>;
+  wasteCards: CardProps[];
+}) => {
+
+  const uTableau = [...tableauCards];
+  const uSuit = [...suitCards];
+  let uWaste = [...wasteCards];
+  const uFlipped: CardProps[] = [];
+  let failure = true;
+
+  if (!destination) {
+    return {
+      failure,
+      uFlipped,
+      uSuit,
+      uTableau, 
+      uWaste, 
+    };
+  }
+
+  const sParts = source.droppableId.split('-');
+  const dParts = destination.droppableId.split('-');
+
+  const isCol = (parts: string[]) => parts[0] === 'col';
+  const isSuit = (parts: string[]) => parts[0] === 'suit';
+
+  const sColIdx = parseInt(sParts[1]);
+  const dColIdx = parseInt(dParts[1]);
+  const sIdx = source.index;
+
+  if (isCol(sParts)) {
+    if (isCol(dParts) && sColIdx !== dColIdx) {
+      // Moving cards between tableau columns
+      const cardsToMove = uTableau[sColIdx].slice(sIdx);
+      const validMove = isValidTableauMove(cardsToMove, uTableau[dColIdx]);
+      if (validMove) {
+        uTableau[dColIdx] = uTableau[dColIdx].concat(cardsToMove);
+        uTableau[sColIdx] = uTableau[sColIdx].slice(0, sIdx);
+        failure = false;
+      }
+    } else if (isSuit(dParts)) {
+      // Moving cards from tableau to foundation
+      const cardsToMove = uTableau[sColIdx].slice(sIdx);
+      const validMove = isValidFoundationMove(cardsToMove, uSuit[dColIdx], dColIdx);
+      if (validMove) {
+        uSuit[dColIdx] = uSuit[dColIdx].concat(cardsToMove);
+        uTableau[sColIdx] = uTableau[sColIdx].slice(0, sIdx);
+        failure = false;
+      }
+    }
+  } else if (sParts[0] === 'waste') {
+    // Moving cards from waste to tableau or foundation
+    const cardsToMove = uWaste.slice(-1);
+    if (isCol(dParts)) {
+      const validMove = isValidTableauMove(cardsToMove, uTableau[dColIdx]);
+      if (validMove) {
+        uTableau[dColIdx] = uTableau[dColIdx].concat(cardsToMove);
+        uWaste = uWaste.slice(0, -1);
+        failure = false;
+      }
+    } else if (isSuit(dParts)) {
+      const validMove = isValidFoundationMove(cardsToMove, uSuit[dColIdx], dColIdx);
+      if (validMove) {
+        uSuit[dColIdx] = uSuit[dColIdx].concat(cardsToMove);
+        uWaste = uWaste.slice(0, -1);
+        failure = false;
+      }
+    }
+  } else if (isSuit(sParts) && isCol(dParts)) {
+    // Moving cards from foundation to tableau
+    const cardsToMove = uSuit[sColIdx].slice(sIdx);
+    const validMove = isValidTableauMove(cardsToMove, uTableau[dColIdx]);
+    if (validMove) {
+      uTableau[dColIdx] = uTableau[dColIdx].concat(cardsToMove);
+      uSuit[sColIdx] = uSuit[sColIdx].slice(0, sIdx);
+      failure = false;
+    }
+  }
+
+  // flip cards programmatically for saving state later
+  uTableau.forEach((column) => {
+    if (column.length === 0) {
+      return;
+    }
+    const lastCard = column[column.length - 1];
+    if (!lastCard.isFaceUp) {
+      lastCard.isFaceUp = true;
+      uFlipped.push(lastCard);
+    }
+  });
+
+  return {
+    failure,
+    uFlipped,
+    uSuit,
+    uTableau, 
+    uWaste, 
+  };
+
+};
+
+const isValidTableauMove = (cardsToMove: CardProps[], destinationColumn: CardProps[]) => {
+  if (destinationColumn.length === 0) {
+    return cardsToMove[0].rank === 'K'; 
+  }
+  const lastCard = destinationColumn[destinationColumn.length - 1];
+  const firstCardToMove = cardsToMove[0];
+  return (
+    RANKS.indexOf(firstCardToMove.rank) === RANKS.indexOf(lastCard.rank) -1 &&
+    lastCard.red !== firstCardToMove.red
+  );
+};
+
+const isValidFoundationMove = (cardsToMove: CardProps[], foundationPile: CardProps[], suitIndex: number) => {
+  if (foundationPile.length === 0) {
+    return cardsToMove[0].rank === 'A' && cardsToMove[0].suit === Object.keys(SUITS)[suitIndex];
+  }
+  const lastCard = foundationPile[foundationPile.length - 1];
+  const firstCardToMove = cardsToMove[0];
+  return (
+    firstCardToMove.suit === lastCard.suit &&
+    RANKS.indexOf(firstCardToMove.rank) === RANKS.indexOf(lastCard.rank) +1
+  );
+};
+
+// check win 
+export const checkWinningCondition = (tableauCards: CardProps[][], wasteCards: CardProps[], stockCards: CardProps[]) => {
+  // Check if all cards in the tableau are face up
+  const allCardsFaceUp = tableauCards.every((column) => column.every((card) => card.isFaceUp));
+  // Check if the waste & stock pile is empty
+  const wasteEmpty = wasteCards.length === 0;
+  const stockEmpty = stockCards.length === 0;
+  return allCardsFaceUp && wasteEmpty && stockEmpty;
+};
+
+export const moveCardsAfterWin = ({
+  tableauCards,
+  setTableauCards,
+  suitCards,
+  setSuitCards,
+}: 
+  {tableauCards: CardProps[][];
+  setTableauCards: React.Dispatch<React.SetStateAction<CardProps[][]>>;
+  suitCards: Array<CardProps[]>;
+  setSuitCards: React.Dispatch<React.SetStateAction<CardProps[][]>>;
+  }) => {
+
+  const anyCardsLeftInTableau = tableauCards.some(column => column.length > 0);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  tableauCards.forEach((column: any[], columnIndex: number) => {
+    const lastCard = column[column.length-1];
+    if (lastCard) {
+      suitCards.forEach((suit, suitIndex) => {
+        const validMove = isValidFoundationMove([lastCard], suit, suitIndex);
+        if (validMove) {
+          const newFoundation = [...suitCards];
+          const newTableau = [...tableauCards];
+          newFoundation[suitIndex].push(lastCard);
+          newTableau[columnIndex] = newTableau[columnIndex].slice(0, -1);
+          setTableauCards(newTableau);
+          tableauCards = newTableau;
+          setSuitCards(newFoundation);
+        }
+      });
+    }
+  });
+
+  if (anyCardsLeftInTableau) {
+    moveCardsAfterWin({
+      setSuitCards,
+      setTableauCards,
+      suitCards,
+      tableauCards,
+    });
+  } else {
+    return;
+  }
+
+};
+
+export const handleMultipleDrag = (
+  source: any,
+  tableauCards: CardProps[][]
+) => {
+  
+  const sParts = source.source.droppableId.split('-');
+  if (sParts[0] === 'col') {
+    
+    const draggedCards = tableauCards[parseInt(sParts[1])].slice(source.source.index);
+    const draggedCard = document.querySelector(`[data-rbd-draggable-id="${draggedCards[0].id}"]`) as HTMLElement;
+    const translate = draggedCard.style.transform.match(/translate\(((-)?\d*.\d*)px, ((-)?\d*.\d*)px/);
+
+    for (let i = 0; i < draggedCards.length; i++) {
+      const draggedCard = document.querySelector(`[data-rbd-draggable-id="${draggedCards[i].id}"]`) as HTMLElement;
+      if (draggedCard && i > 0 && translate) {
+
+        const x = parseInt(translate[1]);
+        const y = parseInt(translate[3]);
+
+        draggedCard.style.transform = `translate(${x}px, ${y+ window.innerWidth*0.05}px)`;
+        draggedCard.style.zIndex = `${i+5000}`;
+      }
+    }
+  }
+  
+};
+
+export const removeTranslate = (
+  tableauCards: CardProps[][]
+) => {
+  for (let i = 0; i < tableauCards.length; i++) {
+  
+    for (let j = 0; j < tableauCards[i].length; j++) {
+      const draggedCard = document.querySelector(`[data-rbd-draggable-id="${tableauCards[i][j].id}"]`) as HTMLElement;
+      if (draggedCard) {
+        draggedCard.style.transform = '';
+        draggedCard.style.zIndex = '';
+      }
+    }
+  }
+};
